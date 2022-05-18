6bb92491f51d345e02a14d35d6397e8f0e70ade2
ShiroHanaF8 <53520538+ShiroHanaF8@users.noreply.github.com>
2022-05-18 22:31:30 +0900

    add ntp request

--- CLIWrapper/Wrapper.cpp
+++ CLIWrapper/Wrapper.cpp
@@ -1,6 +1,9 @@
 #include "Wrapper.h"
 
 #include <msclr/marshal_cppstd.h>
+
+#include <ctime>
+
 using namespace msclr::interop;
 
 using namespace CSharpLibraly;
@@ -51,9 +54,34 @@ namespace CSWrapper {
 
 	void Wrapper::GetTime(int& year, int& month, int& day)
 	{
-		auto now = System::DateTime::Now;
-		year = now.Year;
-		month = now.Month;
-		day = now.Day;
+		try {
+			auto ipAny = gcnew System::Net::IPEndPoint(System::Net::IPAddress::Any, 0);
+			auto objSck = gcnew System::Net::Sockets::UdpClient(ipAny);
+			auto sdat = gcnew array<System::Byte>(48);
+			sdat[0] = 0xB;
+			objSck->Send(sdat, sdat->GetLength(0), "time.windows.com", 123);
+
+			auto rdat = objSck->Receive(ipAny);
+
+			pin_ptr<byte> rcvPtr = &rdat[40];
+			tm time = {};
+			time_t tmsecond = 
+				(time_t)rdat[40] * 16777216 +
+				(time_t)rdat[41] * 65536 +
+				(time_t)rdat[42] * 256 +
+				(time_t)rdat[43];
+			localtime_s(&time, &tmsecond);
+
+			year = 1900 + time.tm_year - 70; // 1970/1/1 and 1900/1/1
+			month = time.tm_mon + 1;
+			day = time.tm_mday;
+		}
+		catch (...)
+		{
+			auto now = System::DateTime::Now;
+			year = now.Year;
+			month = now.Month;
+			day = now.Day;
+		}
 	}
 }
